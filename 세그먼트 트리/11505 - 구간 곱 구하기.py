79334dff77c2b55ef95618ba3ff3ef264e7058d8
hanhyung <hanhyung6376@naver.com>
2021-09-14 11:08:57 +0900

    .

---
+++ 세그먼트 트리/11505 - 구간 곱 구하기.py
@@ -0,0 +1,54 @@
+import math
+import sys
+
+# start, end => 전체 구간, left, right => 탐색 구간, node => 트리에서의 위치
+def tree_mul(node, start, end, left, right):
+    # 찾는 구간이 아니면
+    if right < start or end < left:
+        return 1
+
+    # 찾는 구간이라면 노드값 반환
+    if left <= start and end <= right:
+        return tree[node]
+
+    mid = (start + end) // 2
+
+    # 왼쪽, 오른쪽 나누어서 재귀
+    return (tree_mul(node * 2, start, mid, left, right) * tree_mul(node * 2 + 1, mid + 1, end, left, right)) % 1000000007
+
+# 트리 값 업데이트
+def update(node, start, end, idx, value):
+    # 범위를 벗어날 경우
+    if idx < start or  end < idx:
+        return
+
+    # 리프 노드
+    if start == end:
+        tree[node] = value
+        return
+
+    mid = (start + end) // 2
+    # 연관된 노드 변경
+    update(node*2, start, mid, idx, value)
+    update(node * 2 + 1, mid + 1, end, idx, value)
+    tree[node] = (tree[node * 2] * tree[node * 2 +1]) % 1000000007
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+n, m, k = map(int, input().split())
+tree_size = 2 ** math.ceil(math.log2(n))
+tree = [0] * (tree_size * 2)
+arr = []
+
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+    update(1, 0, n-1, i, num)
+
+for i in range(m+k):
+    a, b, c = map(int, input().split())
+    if a==1:
+        arr[b-1] = c
+        update(1, 0, n-1, b-1, c)
+    else:
+        print(tree_mul(1, 0, n-1, b-1, c-1))
